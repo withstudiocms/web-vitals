0c9cf9fcad65992e756de32a83808a775cada7e2
Adam Matthiesen <amatthiesen@outlook.com>
2025-07-16 11:32:18 -0700

    Dependency cleanup, and update (#3)
    * initial dep cleanup and configure knip
    
    * cleanup and make test fixture types load correctly
    
    * update web vitals package
    
    * add changeset

---
+++ .changeset/ninety-webs-talk.md
@@ -0,0 +1,7 @@
+---
+"@studiocms/web-vitals": minor
+---
+
+Update web-vitals package from v4.2.4 to v5
+
+Note: FID property no longer exists
---
+++ knip.config.ts
@@ -0,0 +1,25 @@
+import type { KnipConfig } from 'knip';
+
+const config: KnipConfig = {
+	exclude: ['duplicates', 'optionalPeerDependencies'],
+	workspaces: {
+		'.': {
+			ignoreDependencies: ['@changesets/config'],
+			entry: ['.github/workflows/*.yml'],
+		},
+		'packages/test-scripts': {
+			entry: ['cmd/**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}', 'index.{js,cjs,mjs,jsx,ts,cts,mts,tsx}'],
+			project: ['**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}'],
+		},
+		'packages/web-vitals': {
+			entry: ['src/**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}', 'test/**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}'],
+			project: ['**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}'],
+			astro: {
+				entry: ['src/**/*.astro'],
+				project: ['src/**/*.astro'],
+			},
+		}
+	},
+};
+
+export default config;
