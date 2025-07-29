8d6e89bf961e72e086690c3d65de795133a854d8
Adam Matthiesen <amatthiesen@outlook.com>
2025-07-29 03:56:17 -0700

    Fix mis-matched versioning (#13)
    * add console logs for testing
    
    * Add debug logs for received and parsed web vitals metrics
    
    * Update MetricIdSchema regex to match v5 format
    
    * Refactor: Remove debug logs from web vitals metrics endpoint and update test IDs to v5 format
    
    * Fix: Update web-vitals zod regex to resolve version number mismatch

---
+++ .changeset/twelve-bars-type.md
@@ -0,0 +1,5 @@
+---
+"@studiocms/web-vitals": patch
+---
+
+Fix web-vitals zod regex, that was previously failing due to mismatched version number
--- packages/web-vitals/src/schemas.ts
+++ packages/web-vitals/src/schemas.ts
@@ -7,10 +7,10 @@ const MetricTypeSchema = z.enum(['CLS', 'INP', 'LCP', 'FCP', 'TTFB']);
 const MetricIdSchema = z
 	.string()
 	// Match https://github.com/GoogleChrome/web-vitals/blob/main/src/lib/generateUniqueID.ts
-	.regex(/^v4-\d{13}-\d{13}$/)
+	.regex(/^v5-\d{13}-\d{13}$/)
 	// Avoid collecting higher resolution timestamp in ID.
-	// Transforms `'v4-1711484350895-3748043125387'` to `'v4-17114843-3748043125387'`
-	.transform((id) => id.replace(/^(v4-\d{8})\d{5}(-\d{13})$/, '$1$2'));
+	// Transforms `'v5-1711484350895-3748043125387'` to `'v5-17114843-3748043125387'`
+	.transform((id) => id.replace(/^(v5-\d{8})\d{5}(-\d{13})$/, '$1$2'));
 
 /** Shape of the data submitted from clients to the collection API. */
 const ClientMetricSchema = z.object({
